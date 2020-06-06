badcd19bebd00c21961cd034f8142b3583e0c112
Esquinazi <esquinazi.dani@gmail.com>
2020-06-06 14:21:56 -0300

    Refactor useGifs in order to use react-query

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,42 +1,47 @@
-import {useContext, useEffect, useState} from 'react'
-import getGifs from '../services/getGifs'
-import GifsContext from '../context/GifsContext'
+import { useEffect } from 'react';
+import { useInfiniteQuery } from 'react-query';
 
-const INITIAL_PAGE = 0
+import getGifs from '../services/getGifs';
 
-export function useGifs ({ keyword } = { keyword: null }) {
-  const [loading, setLoading] = useState(false)
-  const [loadingNextPage, setLoadingNextPage] = useState(false)
+function flatGroups(data) {
+  let array = [];
+  if (data && !!data.length) {
+    data.forEach((group) => {
+      array = array.concat(group);
+    });
+  }
+  return array;
+}
 
-  const [page, setPage] = useState(INITIAL_PAGE)
-  const {gifs, setGifs} = useContext(GifsContext)
+function getGifsQuery(key, keyword, limit, nextPage = 0) {
+  return getGifs({ limit, page: nextPage, keyword });
+}
 
+export function useGifs({ keyword } = { keyword: null }) {
   // recuperamos la keyword del localStorage
-  const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random'
+  const keywordToUse =
+    keyword || localStorage.getItem('lastKeyword') || 'random';
 
-  useEffect(function () {
-    setLoading(true)
+  const { data, status, fetchMore } = useInfiniteQuery(
+    ['gifs', keywordToUse],
+    [15],
+    getGifsQuery,
+    {
+      getFetchMore: (lastGroup, allGroups) => {
+        return allGroups.length;
+      },
+    }
+  );
 
-    getGifs({ keyword: keywordToUse })
-      .then(gifs => {
-        setGifs(gifs)
-        setLoading(false)
-        // guardamos la keyword en el localStorage
-        localStorage.setItem('lastKeyword', keyword)
-      })
-  }, [keyword, keywordToUse, setGifs])
+  useEffect(
+    function () {
+      // guardamos la keyword en el localStorage
+      localStorage.setItem('lastKeyword', keyword);
+    },
+    [keyword]
+  );
 
-  useEffect(function () {
-    if (page === INITIAL_PAGE) return
-
-    setLoadingNextPage(true)
-
-    getGifs({ keyword: keywordToUse, page })
-      .then(nextGifs => {
-        setGifs(prevGifs => prevGifs.concat(nextGifs))
-        setLoadingNextPage(false)
-      })
-  }, [keywordToUse, page, setGifs])
-
-  return {loading, loadingNextPage, gifs, setPage}
+  const gifs = flatGroups(data);
+  const loading = status === 'loading';
+  return { fetchMore, loading, gifs };
 }
--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,47 +1,52 @@
-import React, {useCallback, useRef, useEffect} from 'react'
-import Spinner from 'components/Spinner'
-import ListOfGifs from 'components/ListOfGifs'
-import {useGifs} from 'hooks/useGifs'
-import useNearScreen from 'hooks/useNearScreen'
-import debounce from 'just-debounce-it'
-import {Helmet} from 'react-helmet'
+import React, { useCallback, useRef, useEffect } from 'react';
+import Spinner from 'components/Spinner';
+import ListOfGifs from 'components/ListOfGifs';
+import { useGifs } from 'hooks/useGifs';
+import useNearScreen from 'hooks/useNearScreen';
+import debounce from 'just-debounce-it';
+import { Helmet } from 'react-helmet';
 
-export default function SearchResults ({ params }) {
-  const { keyword } = params
-  const { loading, gifs, setPage } = useGifs({ keyword })
-  const externalRef = useRef()
-  const {isNearScreen} = useNearScreen({
+export default function SearchResults({ params }) {
+  const { keyword } = params;
+  const { loading, gifs, fetchMore } = useGifs({ keyword });
+  const externalRef = useRef();
+  const { isNearScreen } = useNearScreen({
     externalRef: loading ? null : externalRef,
-    once: false
-  })
+    once: false,
+  });
 
-  const title = gifs ? `${gifs.length} resultados de ${keyword}` : ''
+  const title = gifs ? `${gifs.length} resultados de ${keyword}` : '';
 
-  const debounceHandleNextPage = useCallback(debounce(
-    () => setPage(prevPage => prevPage + 1), 200
-  ), [setPage])
+  const debounceHandleNextPage = useCallback(
+    debounce(() => fetchMore(), 200),
+    []
+  );
 
-  useEffect(function () {
-    if (isNearScreen) debounceHandleNextPage()
-  }, [debounceHandleNextPage, isNearScreen])
+  useEffect(
+    function () {
+      if (isNearScreen) debounceHandleNextPage();
+    },
+    [debounceHandleNextPage, isNearScreen]
+  );
 
-  return <>
-    {loading
-      ? <Spinner />
-      : <>
-        <Helmet>
-          <title>{title}</title>
-          <meta name="description" content={title} />
-          <meta name="rating" content="General" />
-        </Helmet>
-        <div className="App-wrapper">
-          <h3 className="App-title">
-            {decodeURI(keyword)}
-          </h3>
-          <ListOfGifs gifs={gifs} />
-          <div id="visor" ref={externalRef}></div>
-        </div>
-      </>
-    }
-  </>
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Helmet>
+            <title>{title}</title>
+            <meta name="description" content={title} />
+            <meta name="rating" content="General" />
+          </Helmet>
+          <div className="App-wrapper">
+            <h3 className="App-title">{decodeURI(keyword)}</h3>
+            <ListOfGifs gifs={gifs} />
+            <div id="visor" ref={externalRef}></div>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
